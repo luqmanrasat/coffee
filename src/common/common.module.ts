f61cf5acefdcad723f92889c406ea5da484ee5c2
Luqman Rasat <luqmanrasat@hotmail.com>
2022-07-12 10:02:19 +0800

    wrap response data inside data property

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,15 +1,17 @@
 import { Module, ValidationPipe } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { APP_FILTER, APP_GUARD, APP_PIPE } from '@nestjs/core';
+import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
 import envConfig from 'src/config/env.config';
 import { HttpExceptionFilter } from './filters/http-exception.filter';
 import { ApiKeyGuard } from './guards/api-key.guard';
+import { WrapResponseInterceptor } from './interceptors/wrap-response.interceptor';
 
 @Module({
   imports: [ConfigModule.forFeature(envConfig)],
   providers: [
     { provide: APP_FILTER, useClass: HttpExceptionFilter },
     { provide: APP_GUARD, useClass: ApiKeyGuard },
+    { provide: APP_INTERCEPTOR, useClass: WrapResponseInterceptor },
   ],
 })
 export class CommonModule {}
